f92a94ffdd9d5eb9ef86567c633e9a0e11453951
Valera Rozuvan <valera.rozuvan@gmail.com>
2017-03-28 11:41:46 +0300

    Added gulp infrastructure to run tests and linting.

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,20 @@
 
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
+var eslint = require('gulp-eslint');
+var argv = require('yargs').argv;
 
 gulp.task('test', function (cb) {
+  var watch = false;
+
+  if (argv.watch === true) {
+    watch = true;
+  }
+
   gulp.src('./**/*-spec.js', {read: false})
     .pipe(mocha({
-      reporter: 'spec'
+      reporter: 'spec',
+      watch: watch
     }))
     .once('error', function () {
       cb();
@@ -16,4 +25,11 @@ gulp.task('test', function (cb) {
       cb();
       process.exit();
     });
+});
+
+gulp.task('lint', function () {
+  return gulp.src(['**/*.js', '!node_modules/**'])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
 });
--- test/main-spec.js
+++ test/main-spec.js
@@ -4,6 +4,10 @@ var chai = require('chai');
 var sinon = require('sinon');
 var sinonChai = require('sinon-chai');
 
+var mainModule = require('../lib/main');
+
+var expect = chai.expect;
+
 chai.should();
 chai.use(sinonChai);
 
@@ -18,5 +22,25 @@ describe('hello', function () {
     hello('foo', cb);
 
     cb.should.have.been.calledWith('hello foo');
+  });
+});
+
+describe('main module', function () {
+  var retVal;
+
+  beforeEach(function () {
+    retVal = null;
+  });
+
+  it('parse returns correct object', function () {
+    retVal = mainModule.parse();
+
+    expect(retVal).to.be.an('object');
+  });
+
+  it('parse returns an object with correct "a" property', function () {
+    retVal = mainModule.parse();
+
+    expect(retVal.a).to.equal(42);
   });
 });
